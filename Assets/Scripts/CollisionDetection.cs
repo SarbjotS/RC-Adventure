907d52c1542d62c01a783a0d015e2fc9b2c58b31
Caanite <61674541+Caanite@users.noreply.github.com>
2021-10-18 00:09:07 +1300

    Added Score for Obstacles and Flags

--- Assets/Scripts/CollisionDetection.cs
+++ Assets/Scripts/CollisionDetection.cs
@@ -11,14 +11,19 @@ public class CollisionDetection : MonoBehaviour
         
         if (other.tag == "Flags")
         {
-            Score.scoreModifier = 10000000000;
+            Score.scoreModifier = 1000;
             Destroy(other.gameObject);
+            Score.AddScore();
         }else if(other.tag == "Cone")
         {
+            Score.scoreModifier = -100;
             Destroy(other.gameObject);
+            Score.AddScore();
         }else if( other.gameObject.tag == "Pothole")
         {
+            Score.scoreModifier = -500;
             Destroy(other.gameObject);
+            Score.AddScore();
         }
     }
 }
--- Assets/Scripts/VictoryLine.cs
+++ Assets/Scripts/VictoryLine.cs
@@ -16,20 +16,19 @@ public class VictoryLine : MonoBehaviour
     private float TimeTaken = 0.0f;
     private float Score = 0.0f;
     public float scoreModifier = 0;
+
+    public void AddScore()
+    {
+        Score = Score + scoreModifier;
+        //Debug.Log("Score Added or Detracted");
+    }
+
     void OnTriggerEnter()
     {
-<<<<<<< Updated upstream
-        VictoryText.SetActive(true);
-        TimeTaken = Timer.GetTimer();
-        Score = 12000.0f - (TimeTaken*100.0f);
-        Score += scoreModifier;
-        scoreText.text = Score.ToString();
-=======
         VictoryText.SetActive(true); //reveal UI
         TimeTaken = Timer.GetTimer(); //get Time from the Timer script
-        Score = 12000.0f - (TimeTaken*100.0f); //calculate score
+        Score = Score + 12000.0f - (TimeTaken*100.0f); //calculate score
         scoreText.text = Score.ToString(); //display score
->>>>>>> Stashed changes
         ScoreContainer.SetActive(true);
         ContinueButton.SetActive(true); //reveal rest of UI
         Destroy(this.gameObject); //destroy object to ensure it doesn't repeat
